ab3b37bbee3392715a32506799d40df351e49929
kawer3343 <glebgalyant@icloud.com>
2025-04-05 10:15:15 +0300

    Initial commit

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+
+
+string filePath = "GameLibrary.txt";
+
+if (File.Exists(filePath))
+{   
+    Console.WriteLine("Сколько игр хотите добавить?: ");
+    int countGame = int.Parse(Console.ReadLine());
+    for (int quantity = 1; quantity <= countGame; quantity++){
+        Console.Write("Введите жанр игры: ");
+        string genre = Console.ReadLine();
+        Console.Write("Введите название игры: ");
+        string name = Console.ReadLine();
+        Console.Write("Введите разработчика игры: ");
+        string dev = Console.ReadLine();
+        string game = $"Жанр: {genre}, название игры: {name}, разработчик: {dev}";
+        File.AppendAllText(filePath, game + "\n");
+        string content = File.ReadAllText(filePath);
+        Console.WriteLine(content);
+    }
+}
+else
+{
+    Console.WriteLine("Файл не найден.");
+}
